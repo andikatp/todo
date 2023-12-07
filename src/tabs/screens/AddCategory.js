b835fd2e73c4bfb4c05e1f924b05deb012d8fc90
bransyahtan <bransyahhhh@gmail.com>
2023-12-07 15:42:23 +0700

    ubah jadi stylesheet

--- src/tabs/screens/AddCategory.js
+++ src/tabs/screens/AddCategory.js
@@ -6,8 +6,9 @@ import {
   TouchableOpacity,
   FlatList,
   ScrollView,
+  StyleSheet,
 } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome"; // Anda perlu menginstal react-native-vector-icons sebelum menggunakan ini
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const AddCategory = () => {
   const [selectedValue, setSelectedValue] = useState(null);
@@ -26,19 +27,11 @@ const AddCategory = () => {
   };
 
   return (
-    <ScrollView contentContainerStyle={{ padding: 15 }}>
-      <Text style={{ fontSize: 30, fontWeight: "bold" }}>Add Category</Text>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.heading}>Add Category</Text>
 
       <TextInput
-        style={{
-          backgroundColor: "#cfd0d1",
-          marginTop: 20,
-          borderRadius: 10,
-          padding: 15,
-          borderWidth: 1,
-          borderColor: "black",
-          fontSize: 17,
-        }}
+        style={styles.input}
         placeholder="Name"
         placeholderTextColor="#9fa0a1"
         onChangeText={(text) => setSelectedValue(text)}
@@ -48,63 +41,23 @@ const AddCategory = () => {
         onPress={() => {
           addCategory(selectedValue);
         }}
-        style={{
-          backgroundColor: "#FF5555",
-          padding: 15,
-          borderRadius: 10,
-          marginTop: 20,
-        }}
+        style={styles.addButton}
       >
-        <Text
-          style={{
-            color: "white",
-            textAlign: "center",
-            fontWeight: "bold",
-            fontSize: 17,
-          }}
-        >
-          ADD CATEGORY
-        </Text>
+        <Text style={styles.buttonText}>ADD CATEGORY</Text>
       </TouchableOpacity>
 
-      <Text style={{ fontSize: 30, fontWeight: "bold", marginTop: 20 }}>
-        List Category
-      </Text>
+      <Text style={styles.heading}>List Category</Text>
 
-      <View
-        style={{
-          flexDirection: "row",
-          flexWrap: "wrap",
-          justifyContent: "flex-start",
-          alignItems: "flex-start",
-        }}
-      >
+      <View style={styles.categoryList}>
         {categories.map((category, index) => (
-          <View
-            key={index}
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              marginRight: 10,
-              marginBottom: 10,
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 20,
-                backgroundColor: "#e0e0e0",
-                padding: 10,
-                borderRadius: 5,
-              }}
-            >
-              {category}
-            </Text>
+          <View key={index} style={styles.categoryItem}>
+            <Text style={styles.categoryText}>{category}</Text>
             <TouchableOpacity onPress={() => removeCategory(index)}>
               <Icon
                 name="times"
                 size={20}
                 color="#FF5555"
-                style={{ marginLeft: 5 }}
+                style={styles.icon}
               />
             </TouchableOpacity>
           </View>
@@ -113,5 +66,57 @@ const AddCategory = () => {
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+  },
+  heading: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#cfd0d1",
+    marginTop: 20,
+    borderRadius: 10,
+    padding: 15,
+    borderWidth: 1,
+    borderColor: "black",
+    fontSize: 17,
+  },
+  addButton: {
+    backgroundColor: "#FF5555",
+    padding: 15,
+    borderRadius: 10,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 17,
+  },
+  categoryList: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+  },
+  categoryItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginRight: 10,
+    marginBottom: 10,
+  },
+  categoryText: {
+    fontSize: 20,
+    backgroundColor: "#e0e0e0",
+    padding: 10,
+    borderRadius: 5,
+  },
+  icon: {
+    marginLeft: 5,
+  },
+});
 
 export default AddCategory;
--- src/tabs/screens/ListTodo.js
+++ src/tabs/screens/ListTodo.js
@@ -29,57 +29,26 @@ const List = ({ navigation }) => {
   };
 
   return (
-    <View style={{ padding: 10 }}>
-      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-        <Text style={{ fontSize: 30, fontWeight: "bold" }}>
-          Hi {user["name"]}
-        </Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Hi {user["name"]}</Text>
         <TouchableOpacity onPress={onSignOut}>
           <Image
             source={require("../../assets/logo.jpg")}
-            style={{
-              borderRadius: 100,
-              width: 50,
-              height: 50,
-            }}
+            style={styles.logo}
           />
         </TouchableOpacity>
       </View>
       <View>
         <TextInput
-          style={{
-            backgroundColor: "#cfd0d1",
-            marginTop: 8,
-            borderRadius: "15",
-            padding: 8,
-            borderwidth: "5px",
-            borderColor: "gray",
-            borderRadius: 10,
-            fontSize: 17,
-          }}
+          style={styles.searchInput}
           placeholder="Search List..."
           placeholderTextColor="#9fa0a1"
         />
       </View>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
+      <View style={styles.pickerContainer}>
         <Picker
-          style={{
-            flex: 1,
-            backgroundColor: "#cfd0d1",
-            marginTop: 15,
-            borderRadius: "15",
-            padding: 2,
-            borderWidth: 1,
-            borderColor: "gray",
-            borderRadius: 10,
-            fontSize: 17,
-            marginRight: 7,
-          }}
+          style={styles.picker}
           placeholderTextColor="#9fa0a1"
           selectedValue={selectedValue}
           onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
@@ -88,24 +57,14 @@ const List = ({ navigation }) => {
             label="Category"
             value={null}
             enabled={false}
-            style={{ color: "#9fa0a1" }}
+            style={styles.pickerItem}
           />
           <Picker.Item label="Category 1" value="Category1" />
           <Picker.Item label="Category 2" value="Category2" />
           <Picker.Item label="Category 3" value="Category3" />
         </Picker>
         <Picker
-          style={{
-            flex: 1,
-            backgroundColor: "#cfd0d1",
-            marginTop: 15,
-            borderRadius: "15",
-            padding: 10,
-            borderwidth: "5px",
-            borderColor: "gray",
-            borderRadius: 10,
-            fontSize: 17,
-          }}
+          style={styles.picker}
           placeholderTextColor="#9fa0a1"
           selectedValue={selectedValue}
           onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
@@ -114,76 +73,38 @@ const List = ({ navigation }) => {
             label="Status"
             value={null}
             enabled={false}
-            style={{ color: "#9fa0a1" }}
+            style={styles.pickerItem}
           />
           <Picker.Item label="Category 1" value="Category1" />
           <Picker.Item label="Category 2" value="Category2" />
           <Picker.Item label="Category 3" value="Category3" />
         </Picker>
       </View>
-      <View style={{ marginTop: "1rem" }}>
-        <View
-          style={{
-            width: "auto",
-            height: 100,
-            backgroundColor: "#b0e9f7",
-            borderRadius: 10,
-            borderWidth: 1,
-            borderColor: "#b0e9f7",
-            padding: 5,
-          }}
-        >
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-              marginTop: 5,
-            }}
-          >
-            <View>
-              <Text
-                style={[styles.todoText, isSelected1 && styles.strikethrough]}
-              >
-                Study - React
-              </Text>
-              <Text style={[isSelected1 && styles.strikethrough]}>
-                Ampun Sepuh Aku Baru Kenal
-              </Text>
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  marginTop: 7,
-                }}
-              >
-                <Icon name="calendar" size={20} color="#82817c" />
-                <Text style={{ marginLeft: 8 }}>1 Desember 2023</Text>
-              </View>
+      <View style={styles.listItem}>
+        <View style={styles.listItemContent}>
+          <View>
+            <Text
+              style={[styles.todoText, isSelected1 && styles.strikethrough]}
+            >
+              Study - React
+            </Text>
+            <Text style={[isSelected1 && styles.strikethrough]}>
+              Ampun Sepuh Aku Baru Kenal
+            </Text>
+            <View style={styles.dateContainer}>
+              <Icon name="calendar" size={20} color="#82817c" />
+              <Text style={styles.dateText}>1 Desember 2023</Text>
             </View>
-            <View style={{ alignItems: "center" }}>
-              <View
-                style={{
-                  width: 60,
-                  height: 20,
-                  backgroundColor: "#eefaa7",
-                  borderRadius: 10,
-                  alignItems: "center",
-                }}
-              >
-                <Text>Study</Text>
-              </View>
-              <CheckBox
-                value={isSelected1}
-                onValueChange={setSelection1}
-                style={{
-                  width: 20,
-                  height: 20,
-                  borderRadius: 10,
-                  marginTop: 10,
-                }}
-              />
+          </View>
+          <View style={styles.checkboxContainer}>
+            <View style={styles.tag}>
+              <Text>Study</Text>
             </View>
+            <CheckBox
+              value={isSelected1}
+              onValueChange={setSelection1}
+              style={styles.checkBox}
+            />
           </View>
         </View>
       </View>
@@ -194,6 +115,61 @@ const List = ({ navigation }) => {
 export default List;
 
 const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  headerText: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  logo: {
+    borderRadius: 100,
+    width: 50,
+    height: 50,
+  },
+  searchInput: {
+    backgroundColor: "#cfd0d1",
+    marginTop: 8,
+    borderRadius: 10,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: "gray",
+    fontSize: 17,
+  },
+  pickerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  picker: {
+    flex: 1,
+    backgroundColor: "#cfd0d1",
+    marginTop: 15,
+    borderRadius: 10,
+    padding: 2,
+    borderWidth: 1,
+    borderColor: "gray",
+    fontSize: 17,
+    marginRight: 7,
+  },
+  pickerItem: {
+    color: "#9fa0a1",
+  },
+  listItem: {
+    marginTop: "1rem",
+  },
+  listItemContent: {
+    width: "auto",
+    height: 100,
+    backgroundColor: "#b0e9f7",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#b0e9f7",
+    padding: 5,
+  },
   todoText: {
     fontSize: 16,
     fontWeight: "bold",
@@ -201,7 +177,28 @@ const styles = StyleSheet.create({
   strikethrough: {
     textDecorationLine: "line-through",
   },
-  item: {
-    fontSize: 9,
+  dateContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 7,
+  },
+  dateText: {
+    marginLeft: 8,
+  },
+  checkboxContainer: {
+    alignItems: "center",
+  },
+  tag: {
+    width: 60,
+    height: 20,
+    backgroundColor: "#eefaa7",
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  checkBox: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    marginTop: 10,
   },
 });
